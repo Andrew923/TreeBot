c4adcb90f7d979f99344705ea5c55287d92d4cfb
andrew yu <andrewyu41213@gmail.com>
2022-04-12 20:04:16 -0500

    blah

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import discord
 import random
+from pokedex import pokedex
 
 client = discord.Client(intents=discord.Intents.all())
+pokedex = pokedex.Pokedex()
 
 @client.event
 async def on_ready():
@@ -15,10 +17,17 @@ async def on_message(message):
     if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
 
+    if message.content.startswith('!say'):
+        await message.channel.send(message.content.replace('!say', ''))
+
+    if message.content.startswith('!pokemon'):
+        pokemon = pokedex.get_pokemon_by_number(random.randint(1,807))[0]
+        await message.channel.send("Good fucking job, " + message.author.mention + " you caught " + pokemon['name'] + "!")
+        await message.channel.send(pokemon['sprite'])
+
     if message.content.startswith('!message'):
-        messages = await message.channel.history(limit=500).flatten()
+        messages = await message.channel.history(limit=300).flatten()
         await message.channel.send(random.choice(messages).content)
-        await message.channel.send(random.choice(messages).embeds[0].to_dict())
 
 
 client.run('OTYyMDUyNTUzMjIxMjE4MzA0.YlB7Qg.UUspoQO4rq8_1ea-eOAYAxtUmmU')
