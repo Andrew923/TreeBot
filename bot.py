524746553a3e2b28f2bb7a36ff6f10b541888f9f
andrew yu <andrewyu41213@gmail.com>
2022-04-13 10:36:07 -0500

    blah

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 import random
 from pokedex import pokedex
+from datetime import datetime
+import json
 
 client = discord.Client(intents=discord.Intents.all())
 pokedex = pokedex.Pokedex()
@@ -14,7 +16,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
+    if message.content.startswith('hello'):
         await message.channel.send('Hello!')
 
     if message.content.startswith('!say'):
@@ -26,8 +28,30 @@ async def on_message(message):
         await message.channel.send(pokemon['sprite'])
 
     if message.content.startswith('!message'):
-        messages = await message.channel.history(limit=300).flatten()
-        await message.channel.send(random.choice(messages).content)
+        message = random.choice(message.channel.history(limit=300).flatten())
+        await message.channel.send(message.content + '\n' + message.jump_url)
+
+    if message.content.startswith('!time'):
+        if (random.randint(1,3) != 1):
+            await message.channel.send("The time is : " + datetime.now().strftime("%H:%M:%S"))
+        else:
+            await message.channel.send("time for you to get a watch hahaha")
+
+    if message.content.startswith('!store'):
+        stored_message = message.content.replace('!store', '')
+        with open('storage.json') as f:
+            storage = json.load(f)
+        storage[str(message.author.id)] = stored_message
+        with open('storage.json', 'w') as f:
+            json.dump(storage, f)
+        await message.channel.send("Stored: " + stored_message)
+    
+    if message.content.startswith('!retrieve'):
+        with open('storage.json') as f:
+            storage = json.load(f)
+        await message.channel.send(storage[str(message.author.id)])
+        
+
 
 
 client.run('OTYyMDUyNTUzMjIxMjE4MzA0.YlB7Qg.UUspoQO4rq8_1ea-eOAYAxtUmmU')
--- test.py
+++ test.py
@@ -1,14 +1,2 @@
-from pokedex import pokedex
-import random
-pokedex = pokedex.Pokedex()
-pokemon = pokedex.get_pokemon_by_number(random.randint(1,807))[0]
-print(pokemon['name'])
-
-
-#html session stuff that didn't work
-# from requests_html import HTMLSession
-# URL = "http://toastybot.com/inventory?id=177962211841540097&name=AndrewYu&avatar=https://cdn.discordapp.com/avatars/177962211841540097/891d6c6089bb63597a9d978054ee500a.webp"
-# session = HTMLSession()
-# r = session.get(URL)
-# r.html.render()
-# print(r.html.text)
+from datetime import datetime
+print("Local time:" + datetime.now().strftime("%H:%M:%S"))
